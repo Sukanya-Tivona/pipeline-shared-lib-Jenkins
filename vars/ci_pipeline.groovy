b70702260dd7b353bb22b2566b92dea03bef3930
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-26 11:18:29 +0530

    Update ci_pipeline.groovy

--- vars/ci_pipeline.groovy
+++ vars/ci_pipeline.groovy
@@ -1,8 +1,23 @@
-//def call( String IMAGEVERSION, String projectname,  String AWS_DEFAULT_REGION,  String AWS_ACCOUNT_ID ){
-def call(Map config = [:]) {
+//def call( String IMAGEVERSION, String projectname,  String AWS_DEFAULT_REGION,  String AWS_ACCOUNT_ID,
+//AWS_ACCESS_KEY_ID = env.AWS_ACCESS_KEY_ID ,AWS_SECRET_ACCESS_KEY = env.AWS_SECRET_ACCESS_KEY,PAT = env.PAT ){
+def call(Map pipelineParams) {
 
 pipeline{
+
     agent any 
+    environment
+       {
+        //AWS_ACCOUNT_ID="562922379100"
+        //AWS_DEFAULT_REGION="us-west-2"
+        AWS_ACCESS_KEY_ID     = credentials('aws_pratice')
+        AWS_SECRET_ACCESS_KEY = credentials('aws_pratice')
+        PAT = credentials('PAT')
+        //APPLICATION_URL = "https://$PAT@github.com/Observe-Life-AI/ol-services-node.git"
+        
+        REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+        //APPLICATION_REPO_URL = ""
+        //http://562922379100.dkr.ecr.us-west-2.amazonaws.com/devtools1
+        }
     stages 
     {
        stage('Checkout') 
@@ -14,13 +29,13 @@ pipeline{
            '''
 
             //sh 'git clone -b main ${APPLICATION_URL} '
-               }
+            }
        }
         
      
        stage('AWS configure and ecr login ') 
        {
-       steps { sh '''
+       steps{ sh '''
               aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
               aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
               aws configure set default.region $config.AWS_DEFAULT_REGION
@@ -28,7 +43,7 @@ pipeline{
               docker login --username AWS --password-stdin ${config.AWS_ACCOUNT_ID}.dkr.ecr.${config.AWS_DEFAULT_REGION}.amazonaws.com
                '''
          
-              }
+            }
        }
     
        //locally run
@@ -42,12 +57,12 @@ pipeline{
               def IMAGENAMES = ['data-read', 'data-write', 'timelines']
               for (IMAGENAME in IMAGENAMES) 
               {  
-                sh "cp $HOME/workspace/$config.projectname/ol-container-images-node/Dockerfile $HOME/workspace/$config.projectname/ol-services-node/ol-node-api-$IMAGENAME"
-                  dir("$HOME/workspace/$config.projectname/ol-services-node/ol-node-api-${IMAGENAME}")
+                sh "cp $HOME/workspace/$pipelineParams.projectname/ol-container-images-node/Dockerfile $HOME/workspace/$pipelineParams.projectname/ol-services-node/ol-node-api-$IMAGENAME"
+                  dir("$HOME/workspace/$pipelineParams.projectname/ol-services-node/ol-node-api-${IMAGENAME}")
                   {    
-                      sh "docker build -t ${REPOSITORY_URI}/${IMAGENAME}:${config.IMAGEVERSION} ."
+                      sh "docker build -t ${REPOSITORY_URI}/${IMAGENAME}:${pipelineParams.IMAGEVERSION} ."
                     // sh "docker tag ${IMAGENAME} ${REPOSITORY_URI}:${IMAGEVERSION}"
-                      sh "docker push ${REPOSITORY_URI}/${IMAGENAME}:${config.IMAGEVERSION} "
+                      sh "docker push ${REPOSITORY_URI}/${IMAGENAME}:${pipelineParams.IMAGEVERSION} "
                   }
               }
        }
