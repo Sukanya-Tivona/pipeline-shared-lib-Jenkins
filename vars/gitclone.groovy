1401115589f157e66ee6c05828ec706334673b14
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-26 14:48:01 +0530

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,26 +1,4 @@
-def call() {
-    pipeline{
-      environment
-     {
-      
-      PAT = credentials('PAT')
-    
-      }
-      agent any
-    stages 
-    {
-       stage('Checkout') 
-       {
-       steps {
-           
-           git branch: 'main', credentialsId: 'My_Github_Tivona_login', url: 'https://$PAT@github.com/Observe-Life-AI/ol-services-node.git'
-          
-           git branch: 'feature', credentialsId: 'My_Github_Tivona_login', url: 'https://$PAT@github.com/Observe-Life-AI/ol-container-images-node.git'
-           
-          }
-       }
-    }
-    }
-}
-        
-     
+def call(name )
+{
+  echo "Hello world  $name"
+} 
