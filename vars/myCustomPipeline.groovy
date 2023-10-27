3df4d5a405bf1ace28f2e339f71725ddb877d4a5
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 12:22:32 +0530

    Create myCustomPipeline.groovy

---
+++ vars/myCustomPipeline.groovy
@@ -0,0 +1,45 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+
+        environment {
+            AWS_ACCESS_KEY_ID     = credentials(pipelineParams.awsAccessKeyId)
+            AWS_SECRET_ACCESS_KEY = credentials(pipelineParams.awsSecretAccessKey)
+            REPOSITORY_URI = "${pipelineParams.awsAccountId}.dkr.ecr.${pipelineParams.awsRegion}.amazonaws.com"
+        }
+
+        parameters {
+            string(name: 'IMAGEVERSION', defaultValue: '1.0', description: 'Version number to build for')
+            string(name: 'projectname', defaultValue: 'yourpipelinename', description: 'name of the pipeline')
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    script {
+                        checkoutGit(pipelineParams.pat)
+                    }
+                }
+            }
+
+            stage('AWS configure and ECR login') {
+                steps {
+                    awsConfigureAndEcrLogin(pipelineParams)
+                }
+            }
+
+            stage('Build and Push Docker Images') {
+                steps {
+                    buildAndPushDockerImages(pipelineParams)
+                }
+            }
+        }
+
+        post {
+            cleanup {
+                echo "Clean up in post workspace"
+                cleanWs()
+            }
+        }
+    }
+}
