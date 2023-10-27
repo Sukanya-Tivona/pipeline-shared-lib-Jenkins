5f7ca34a0f8ce56164d07123e7f94e13d60deb4c
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 12:00:55 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
-def call(String repo, String branch) {
-  echo "||------ CLONING $repo ------||"
-  
-    git branch: branch, changelog: false, credentialsId: 'My_Github_Tivona_login', poll: false, url: "git@github.com:myprivateorg/$repo.git"
-  
+def call(String repoUrl, String branch){
+   def workingDir = "${env.WORKSPACE}"
+   sh "git clone ${repoUrl} ${workingDir}"
+   sh "git checkout ${branch}"
+   return workingDir
 }
