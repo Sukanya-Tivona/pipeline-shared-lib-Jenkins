2e03004e252366597a5ab8c13ad772e9b2e2d598
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 14:22:11 +0530

    Update myCustomPipeline.groovy

--- vars/myCustomPipeline.groovy
+++ vars/myCustomPipeline.groovy
@@ -6,6 +6,7 @@ def call(Map pipelineParams) {
             AWS_ACCESS_KEY_ID     = credentials(pipelineParams.awsAccessKeyId).toString()
             AWS_SECRET_ACCESS_KEY = credentials(pipelineParams.awsAccessKeyId).toString()
             REPOSITORY_URI = "${pipelineParams.awsAccountId}.dkr.ecr.${pipelineParams.awsRegion}.amazonaws.com"
+            PAT = credentials(pipelineParams.PAT).toString()
         }
 
         parameters {
