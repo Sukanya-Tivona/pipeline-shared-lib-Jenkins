29c55ac601edcf3a0b9119f5a56bd80d78e99ae2
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 10:57:26 +0530

    Update practice.groovy

--- vars/practice.groovy
+++ vars/practice.groovy
@@ -1,4 +1,17 @@
 #!/usr/bin/env groovy
-def call(String name ) {
-  echo "Hello, ${name},how are you ?"
+// def call(String name ) {
+//   echo "Hello, ${name},how are you ?"
+// }
+// shared-library/vars/cloneGitRepo.groovy
+
+def call(Map config) {
+    def gitURL = config.gitURL
+    def branch = config.branch ?: 'master'
+    def destination = config.destination ?: '.'
+
+    checkout([$class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: gitURL]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: destination]]])
 }
