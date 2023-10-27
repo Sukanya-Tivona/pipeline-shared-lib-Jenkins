ef7f36be655e56ba8911053a2693248d534dcb5a
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 12:57:20 +0530

    Update checkoutGit.groovy

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,4 +1,4 @@
-def checkoutGit(pat) {
+def checkoutGit(String pat) {
     sh """
         git clone -b main https://${pat}@github.com/Observe-Life-AI/ol-services-node.git
         git clone -b feature/OLMS-54-initial-node-container https://${pat}@github.com/Observe-Life-AI/ol-container-images-node.git
