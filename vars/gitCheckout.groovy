88ef5d360e15a96fd2754b4252cf0045fadb82aa
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 11:43:59 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,6 +2,6 @@
 def call(String repo, String branch) {
   echo "||------ CLONING $repo ------||"
   
-    git branch: branch, changelog: false, credentialsId: 'jenkinsgit', poll: false, url: "git@github.com:myprivateorg/$repo.git"
+    git branch: branch, changelog: false, credentialsId: 'My_Github_Tivona_login', poll: false, url: "git@github.com:myprivateorg/$repo.git"
   
 }
