7eb57a32c22a8a4bf6498e55bb6eaf14f6f2827d
Sukanya-Tivona <133114334+Sukanya-Tivona@users.noreply.github.com>
2023-10-27 11:01:36 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,12 @@
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+#!/usr/bin/env groovy
+def call(Map config) {
+    def gitURL = config.gitURL
+    def branch = config.branch ?: 'master'
+    def destination = config.destination ?: '.'
+
+    checkout([$class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: gitURL]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: destination]]])
 }
